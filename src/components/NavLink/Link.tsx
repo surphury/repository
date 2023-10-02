279c5c9be87c8d7a0cde4634afc72675f093bec6
José Berríos <surphury@gmail.com>
2023-10-01 22:15:08 -0500

    finish?

--- src/components/NavLink/Link.tsx
+++ src/components/NavLink/Link.tsx
@@ -15,7 +15,13 @@ export function Link({
   return (
     <NavLink
       to={to}
-      className={'text-base' + ' ' + className + ' ' + activeClassName}
+      className={
+        'text-base block py-2 pr-4 pl-3 md:p-0 border-b border-gray-100 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-cyan-700 md:dark:hover:bg-transparent md:dark:hover:text-white' +
+        ' ' +
+        className +
+        ' ' +
+        activeClassName
+      }
     >
       {children}
     </NavLink>
